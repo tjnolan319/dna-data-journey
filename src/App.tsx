cebe630b04540f421f74dad78455000f5f6c730d
Tim Nolan <tjnolan319@gmail.com>
2025-06-25 13:46:04 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/dna-data-journey">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/genre-category-project" element={<GenreCategoryProject />} />
