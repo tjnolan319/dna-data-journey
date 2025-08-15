0df4dffe4815499fcb6a631af2d62f9f5e9b4c8d
Tim Nolan <tjnolan319@gmail.com>
2025-08-15 11:55:58 -0400

    Update PortfolioSkillsDashboard.tsx

--- src/pages/PortfolioSkillsDashboard.tsx
+++ src/pages/PortfolioSkillsDashboard.tsx
@@ -454,7 +454,7 @@ const PortfolioSkillsDashboard = () => {
 
                     <div className="grid lg:grid-cols-3 gap-8">
                         <div className="lg:col-span-2">
-                            <Card className="h-full bg-gray-400">
+                            <Card className="h-full bg-white">
                                 <CardHeader>
                                     <CardTitle className="flex items-center justify-between">
                                           <span>Interdisciplinary Strengths Venn-Diagram</span>
